c723c06eaf53aa59f44edc7da1f60ba71d5bf211
Madvin <m.parvin986@gmail.com>
2024-05-23 01:38:41 +0330

    add HasPing func

--- tools/ping_host.go
+++ tools/ping_host.go
@@ -1,1 +1,14 @@
 package tools
+
+import (
+	"net"
+)
+
+func HasPing(host string) bool {
+	conn, err := net.Dial("ip4", host)
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
