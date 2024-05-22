ac82f009e052ae95e8bd1e5cb701aa5ee6a1e884
Madvin <m.parvin986@gmail.com>
2024-05-23 01:13:51 +0330

    add GetHostname function

--- tools/get_hostname.go
+++ tools/get_hostname.go
@@ -1,1 +1,14 @@
 package tools
+
+import (
+	"net/url"
+)
+
+func GetHostname(inputUrl string) string {
+	u, err := url.Parse(inputUrl)
+	if err != nil {
+		return ""
+	}
+	hostName := u.Hostname()
+	return hostName
+}
